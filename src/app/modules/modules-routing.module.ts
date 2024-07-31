2f31c9965703fdd7a79b269654b302e69d2c06de
AllyGB <allygonval348@gmail.com>
2024-07-30 18:19:10 -0600

    Update

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './private/components/sidebar/sidebar.component';
 
 const routes: Routes = [
+  {
+    path: 'app',
+    component: SidebarComponent,
+    loadChildren: () => import('./private/private.module').then(m => m.PrivateModule) 
+  },
   {
     path: '', 
     loadChildren: () => import('./public/public.module').then(m => m.PublicModule) 
